29150587a61510f22d7f8c88811ca52e15eb59a9
ester <estersilvade@gmail.com>
2022-01-02 14:11:53 -0300

    helper

--- src/helper/auth.js
+++ src/helper/auth.js
@@ -1,5 +1,15 @@
 const bcrypt = require('bcrypt')
 
 exports.hashPassWord = async (password, res) => {
-  
+  try {
+    const salt = await bcrypt.genSalt(8)
+
+    const hash = await bcrypt.hash(password, salt)
+
+    return hash
+
+  }catch ( error ){
+    console.log(error)
+    res.status(500).json({message: mensagem.error})
+  }
 }
